68a3e6503541e238e7751acb1390a7eccdb647c8
Artur de Oliveira Tsuda <artur.o.tsuda@gmail.com>
2016-04-08 22:07:58 -0300

    more tests, not passing. Something to do with float regex, maybe?

--- test/json_ex_test.exs
+++ test/json_ex_test.exs
@@ -2,6 +2,23 @@ defmodule JsonExTest do
   use ExUnit.Case
   doctest JsonEx
 
+  test "lexer returns correct single token" do
+    {:ok, [token|_], _} = "\"hello\"" |> to_char_list |> :json_lexer.string
+    assert {:string, 1, "\"hello\""} == token
+
+    {:ok, [token|_], _} = "true" |> to_char_list |> :json_lexer.string
+    assert {:bool, 1, true} == token
+
+    {:ok, [token|_], _} = "null" |> to_char_list |> :json_lexer.string
+    assert {:nil, 1, nil} == token
+
+    {:ok, [token|_], _} = "1" |> to_char_list |> :json_lexer.string
+    assert {:int, 1, 1} == token
+
+    {:ok, [token|_], _} = "1.5" |> to_char_list |> :json_lexer.string
+    assert {:float, 1, 1.5} == token
+  end
+
   test "parses json correctly" do
     assert JsonEx.parse "[]" == []
     assert JsonEx.parse "{}" == %{}
